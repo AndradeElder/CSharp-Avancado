dd30dc6e3a50ffba0b47b8d17077ce7a3c62dee8
AndradeElder <elder_andrade@estudante.sesisenai.org.br>
2023-11-28 21:37:33 -0300

    Inicio do projeto

---
+++ ProjetoTresCamadas/ProjetoEmTresCamadas.Pizzaria.RegraDeNegocio/Pizza.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoEmTresCamadas.Pizzaria.RegraDeNegocio
+{
+    public enum TamanhoDaPizza
+    {
+        Pequena = 0,
+        Media = 1,
+        Grande = 2
+    }
+
+    public class Pizza
+    {
+        public string Sabor { get; set; }
+        public TamanhoDaPizza TamanhoDaPizza { get; set; }
+        public string Descricao { get; set; }
+
+        public Pizza()
+        {
+            
+
+
+        }
+
+        public Pizza CriarPizza(string Sabor, TamanhoDaPizza tamanhoDaPizza, string descricao = "")
+        {
+            Sabor = Sabor;
+            TamanhoDaPizza = tamanhoDaPizza;
+            if (string.IsNullOrEmpty(descricao))
+            {
+                Descricao = descricao;
+            }
+            return this;
+        }
+    }
+}
